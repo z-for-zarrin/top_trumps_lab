493c6e426bdaaa8bebc3da422cf7a8693631d510
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-02-26 15:08:43 +0000

    implemented service into controller so cards can be posted for a game

--- src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
+++ src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -1,6 +1,8 @@
 package com.example.top_trumps_start_code.controllers;
 
 import com.example.top_trumps_start_code.models.Card;
+import com.example.top_trumps_start_code.services.TopTrumpsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +16,12 @@ import java.util.ArrayList;
 @RequestMapping(value = "/top_trumps")
 public class TopTrumpsController {
 
+    @Autowired
+    TopTrumpsService topTrumpsService;
+
     @PostMapping
     public ResponseEntity<String> playGame(@RequestBody ArrayList<Card> cards){
+        String reply = topTrumpsService.checkWinner(cards.get(0), cards.get(1));
         return new ResponseEntity<>(reply, HttpStatus.OK);
     }
 }
